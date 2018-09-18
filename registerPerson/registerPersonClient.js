bd98df89561381c7c49ecc347e9b70dc34f81170
naker90 <AntonioJesusSG96@gmail.com>
2018-09-18 21:47:57 +0100

    implementing client

--- registerPerson/registerPersonClient.js
+++ registerPerson/registerPersonClient.js
@@ -1,7 +1,15 @@
 function client(){
 
-    function registerPerson(){
-        throw new Error("not implemented")
+    function registerPerson(request, successCallback, errorCallback){
+        let xhttp = new XMLHttpRequest();
+        xhttp.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                successCallback();
+            }
+            errorCallback();
+        };
+        xhttp.open("POST", "https://jsonplaceholder.typicode.com/posts", true);
+        xhttp.send(request);
     }
 
     return {
