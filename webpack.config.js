97fbe653c57cdb2b32668dfe85080d511e677981
naker90 <AntonioJesusSG96@gmail.com>
2018-09-23 17:22:52 +0100

    install babel-loader and use it in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
     output: {
         filename: "bundle.js"
     },
-    mode: "production"
+    mode: "production",
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: [{
+                loader: "babel-loader",
+                options: {
+                    presets: ['env']
+                }
+            }]
+        }]
+    },
 };
