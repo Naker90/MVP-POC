8142524bbe16e0bf6f026316c33b3a401a159cef
naker90 <AntonioJesusSG96@gmail.com>
2018-09-23 17:37:25 +0100

    fix client

--- src/registerPerson/registerPersonClient.js
+++ src/registerPerson/registerPersonClient.js
@@ -3,7 +3,7 @@ function client(){
     function registerPerson(request, successCallback, errorCallback){
         let xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
-            if (this.readyState === 4 && this.status === 200) {
+            if (this.readyState === 4 && this.status === 201) {
                 successCallback();
             }
             errorCallback();
